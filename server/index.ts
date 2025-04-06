5ee4b5b7b0a2678de392afbf1c22c0351426234d
alexaldearroyo <alexaldearroyo@gmail.com>
2025-04-07 01:42:57 +0200

    Polish server/index.ts

--- server/index.ts
+++ server/index.ts
@@ -6,25 +6,15 @@ import { registerChatHandlers } from './sockets/chat';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
+  // NOTE: CORS set to '*' for development purposes only.
   cors: { origin: '*' },
 });
 
-// Log number of connected clients every 5 seconds
-setInterval(() => {
-  const connectedClients = io.sockets.sockets.size;
-  console.log(`Connected clients: ${connectedClients}`);
-}, 5000);
 
 io.on('connection', (socket) => {
-  console.log('Client connected:', socket.id);
-  console.log(`Total clients connected: ${io.sockets.sockets.size}`);
 
-  socket.on('disconnect', () => {
-    // TODO: Clean up user session or notify group members (optional enhancement)
-    // TODO: Implement disconnect on the frontend
-    console.log('Client disconnected:', socket.id);
-    console.log(`Total clients connected: ${io.sockets.sockets.size}`);
-  });
+  // TODO: Implement authentication logic
+  // TODO: Implement disconnect logic
 
   registerChatHandlers(io, socket); // Chat logic here
 });
